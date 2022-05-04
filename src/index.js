20fb78d6e237c2e9e80b830046ebaa9843ceeedb
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2022-05-04 18:36:47 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import react from "react";
+import ReactDOM from "react-dom";
 
-import App from "./App";
-
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+ReactDOM.render(
+  <div>
+    <h1>Sanidhya Varshney</h1> <p> A Compter Science Bachelor</p>
+  </div>,
+  document.getElementById("root")
 );
